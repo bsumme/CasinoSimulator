77ea7253278b0659b0d28dbb23695e152c6a29fe
bsumme <bsumm6@gmail.com>
2025-11-08 20:54:19 -0500

    Remove casino simulator assets and focus on NFL injury tracker

--- server/data/promotions.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    id: 'profit-boost-30-token',
-    name: '30% Profit Boost Token',
-    description: 'Redeem a token to increase net winnings by 30% on a single wager.',
-    type: 'profitBoost',
-    boost: 0.3
-  }
-]
